8189ebfbc3064b45edcfd1888cda727c15cd363b
Lakshit Paliwal <lakshit.p@advancesolutions.com>
2025-08-13 18:42:37 +0530

    Version 1.0.0

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,16 +3,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-  // Replace with your Firebase config
-   apiKey: "AIzaSyDiApykSXxGI56L_n4f7xEpvcYevSn0i30",
-  authDomain: "vegvendor-f7bb8.firebaseapp.com",
-  projectId: "vegvendor-f7bb8",
-  storageBucket: "vegvendor-f7bb8.firebasestorage.app",
-  messagingSenderId: "253343272874",
-  appId: "1:253343272874:web:e5dcd0ce425505853765d3",
-  measurementId: "G-22KRNM8D0S"
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
